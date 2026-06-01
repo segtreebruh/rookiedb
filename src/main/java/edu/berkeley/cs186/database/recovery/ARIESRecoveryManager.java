1805213d4f496f1df387fb1bff8c2b2f913ffa54
Cuong Dang <dvucuong2005@gmail.com>
2026-06-01 09:35:17 +0700

    add savepoint

--- src/main/java/edu/berkeley/cs186/database/recovery/ARIESRecoveryManager.java
+++ src/main/java/edu/berkeley/cs186/database/recovery/ARIESRecoveryManager.java
@@ -447,7 +447,7 @@ public class ARIESRecoveryManager implements RecoveryManager {
         long savepointLSN = transactionEntry.getSavepoint(name);
 
         // TODO(proj5): implement
-        return;
+        rollbackToLSN(transNum, savepointLSN);
     }
 
     /**
